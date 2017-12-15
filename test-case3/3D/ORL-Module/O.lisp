732fd02c7d0e6c75feb7163710e4360c503c3165
Askarpour <mehrnoosh.askarpour@gmail.com>
2017-12-15 23:15:39 +0100

    current version

--- test-case3/3D/ORL-Module/O.lisp
+++ test-case3/3D/ORL-Module/O.lisp
@@ -1,10 +1,5 @@
 (defvar operatorNum 2)
-
-; (loop for i in (loop for x from 1 to operatorNum collect x) collect (progn (eval `(define-tvar ,(read-from-string (format nil "Body_Part_pos_~A" i)) *int* *int*))))
-
-
 (defvar body_indexes `(head_area chest_area arm_area leg_area))
-; (defvar body_indexes_for_risk `(head Shoulders Chest Belly Pelvis Upper_Arm Hand Thigh Leg Neck Lower_Arm))
 
 (defun Operator_Body (opID)
  (eval (list `alwf (append `(&&
@@ -25,10 +20,6 @@
  	(always_attached ,(read-from-string (format nil "`operator_~A_~A" opID `head_area)) ,(read-from-string (format nil "`operator_~A_~A" opID `chest_area)))
  	(always_attached ,(read-from-string (format nil "`operator_~A_~A" opID `chest_area)) ,(read-from-string (format nil "`operator_~A_~A" opID `leg_area)))
 
- 	; (!! (In_same_L `Base_1 ,(read-from-string (format nil "`operator_~A_~A" opID `head_area))))
- 	; (!! (In_same_L `Base_1 ,(read-from-string (format nil "`operator_~A_~A" opID `chest_area))))
- 	; (!! (In_same_L `Base_1 ,(read-from-string (format nil "`operator_~A_~A" opID `arm_area))))
- 	; (!! (In_same_L `Base_1 ,(read-from-string (format nil "`operator_~A_~A" opID `leg_area))))
 
 	(forbiden_for_human ,(read-from-string (format nil "~A" opID)) (setq l '(`L_15)))
 
